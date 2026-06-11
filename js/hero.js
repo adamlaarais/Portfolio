db2a8f220a0faed1acb7c7691bd92546855d7fd3
adam <laaraisadam22@gmail.com>
2026-06-11 23:51:25 +0200

    oi

--- js/hero.js
+++ js/hero.js
@@ -21,7 +21,9 @@ export function setHeroInitial() {
   gsap.set('.site-header', { opacity: 0, y: -14 });
   gsap.set('.hero__word > span', { yPercent: 115 });
   gsap.set('.hero__tagline', { opacity: 0, y: 16 });
-  gsap.set(subLines, { yPercent: 120 });
+  if (subLines.length > 0) {
+    gsap.set(subLines, { yPercent: 120 });
+  }
   gsap.set('.hero__social li', { opacity: 0, y: 14 });
   gsap.set('.hero__discover', { opacity: 0 });
   gsap.set('.hero__bracket .bar-v', { scaleY: 0 });
@@ -37,7 +39,9 @@ export function buildHeroIntro(tl, position = 0) {
   t.to('.site-header', { opacity: 1, y: 0, duration: 1, ease: 'power3.out' }, 0.4);
   t.to('.hero__tagline', { opacity: 1, y: 0, duration: 0.7 }, 0);
   t.to('.hero__word > span', { yPercent: 0, duration: 1.05, stagger: 0.1, ease: 'power4.out' }, 0.05);
-  t.to(subLines || [], { yPercent: 0, duration: 0.85, stagger: 0.08 }, 0.4);
+  if (subLines && subLines.length > 0) {
+    t.to(subLines, { yPercent: 0, duration: 0.85, stagger: 0.08 }, 0.4);
+  }
   t.to('.hero__bracket .bar-v', { scaleY: 1, duration: 0.5 }, 0.45);
   t.to('.hero__bracket .bar-h', { scaleX: 1, duration: 0.5 }, 0.6);
   t.to('.hero__social li', { opacity: 1, y: 0, duration: 0.6, stagger: 0.1 }, 0.6);
